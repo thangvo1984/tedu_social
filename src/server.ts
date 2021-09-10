63e6a0a0538b4ffa6cc1c0c8f1bbdb0becfd8a41
ThangVo <thangvo@meditekcorp.com.vn>
2021-09-10 22:00:49 +0800

    structure project

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
-import express, { Request, Response } from "express";
+import App from "./app";
+import { IndexRoute } from "./modules/index";
 
-const port = process.env.PORT || 5000;
+const routes = [new IndexRoute()];
+const app = new App(routes);
 
-const app = express();
+app.listen();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("API is running......");
-});
+// import express, { Request, Response } from "express";
+// import { IndexRoute } from "modules/index";
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+// const port = process.env.PORT || 5000;
+
+// const app = express();
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("API is running......");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`);
+// });
